e30229e040cf71c6c08baa0ac563744d50c9a08d
Andrew Virts <96750426+asvirts@users.noreply.github.com>
2022-09-04 17:15:18 -0500

    v0.03

--- scripts.js
+++ scripts.js
@@ -1,7 +1,17 @@
 // Here's where all the teams go
 
 // Alabama Crimson Tide
+let alabamaRanking = 1
 let alabamaScores = [55]
 let alabamaOpponentScores = [0]
+let alabamaSoS = 1
+document.querySelector("#Alabama").innerHTML = alabamaRanking
+document.querySelector("#alabamaSoS").innerHTML = alabamaSoS
 
-document.querySelector("#Alabama").innerHTML = alabamaScores[0]
+// Auburn Tigers
+let auburnRanking = 2
+let auburnScores = [42]
+let auburnOpponentScores = [16]
+let auburnSoS = 2
+document.querySelector("#Auburn").innerHTML = auburnRanking
+document.querySelector("#auburnSoS").innerHTML = auburnSoS
