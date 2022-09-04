7cb1d460a934714ba24f1db54f4accdd0725da89
Andrew Virts <96750426+asvirts@users.noreply.github.com>
2022-09-04 17:04:40 -0500

    v0.02

--- scripts.js
+++ scripts.js
@@ -1,4 +1,7 @@
 // Here's where all the teams go
 
 // Alabama Crimson Tide
-let alabamaScores = document.getElementById("Alabama").innerHTML = "Roll Tide"
+let alabamaScores = [55]
+let alabamaOpponentScores = [0]
+
+document.querySelector("#Alabama").innerHTML = alabamaScores[0]
