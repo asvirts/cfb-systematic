ff5e38f1c2e8500d115f7340ae041e56d0bee89b
Andrew Virts <96750426+asvirts@users.noreply.github.com>
2022-09-05 16:10:19 -0500

    still setting up

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require('express')
+const dotenv = require('dotenv')
+const morgan = require('morgan')
 
 const app = express()
 
+dotenv.config( { path:'config.env' } )
 const PORT = process.env.PORT || 8080
+
+// log requests
+app.use(morgan('tiny'))
 
 app.get('/', (req, res) => {
     res.send('cfb systematic rankings')
