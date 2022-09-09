2111026ecd246b27ccecb88168d21216dd9cf718
Andrew Virts <96750426+asvirts@users.noreply.github.com>
2022-09-09 14:08:19 -0500

    reset

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const path = require('path')
 
 const app = express()
 
@@ -10,8 +12,21 @@ const PORT = process.env.PORT || 8080
 // log requests
 app.use(morgan('tiny'))
 
+// parse request to body-parser
+app.use(bodyParser.urlencoded( {extended: true} ))
+
+// set view engine
+app.set('view engine', 'ejs')
+// app.set('views', path.resolve(__dirname, 'views'))
+
+// load assets
+app.use('/css', express.static(path.resolve(__dirname, 'assets/css')))
+app.use('/img', express.static(path.resolve(__dirname, 'assets/img')))
+app.use('/js', express.static(path.resolve(__dirname, 'assets/js')))
+
+
 app.get('/', (req, res) => {
-    res.send('cfb systematic rankings')
+    res.render('index')
 })
 
 app.listen(PORT, ()=> { console.log('Server is up and running')})
